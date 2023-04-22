d909392f45b1eadf395dc3fccb1c90cfd5f6d338
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2023-04-22 22:47:39 +0300

    Calculations implemented

--- src/components/TotalCost.js
+++ src/components/TotalCost.js
@@ -1,37 +1,57 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const TotalCost = () => {
+  const { refuels } = useContext(GlobalContext);
+
+  const liters = refuels.map(refuels => Number(refuels.liters))
+  const price = refuels.map(refuels => Number(refuels.price))
+  const distance = refuels.map(refuels => Number(refuels.distance))
 
 
+  const totalLiters = liters
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
 
+  const totalPrice = price
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
 
+  const totalDistance = distance
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
 
+  const averageExpenses = ((totalPrice / totalDistance) * 100)
+    .toFixed(2);
 
-
-
+  const averageConsumption = ((totalLiters / totalDistance) * 100)
+    .toFixed(2);
     
   return (
     <>
         <div>TotalCost</div>
         <div>
             <h4>Total Sum:</h4>
-            <p>000 €</p>
+            <p>{totalPrice} €</p>
         </div>
         <div>
             <h4>Total Consumption:</h4>
-            <p>000 Liters</p>
+            <p>{totalLiters} Liters</p>
         </div>
         <div>
             <h4>Total Distance</h4>
-            <p>000 km</p>
+            <p>{totalDistance} km</p>
         </div>
         <div>
             <h4>Average Expenses</h4>
-            <p>000 € per 100km</p>
+            <p>{averageExpenses} € per 100km</p>
         </div>
         <div>
             <h4>Average Consumption</h4>
-            <p>000 Liters per 100km</p>
+            <p>{averageConsumption} Liters per 100km</p>
         </div>       
     </>
   )
