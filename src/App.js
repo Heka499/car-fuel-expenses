d2c467157bd5188206024e4d3715f2ec24c36f04
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2023-04-22 16:02:29 +0300

    added components and GlobalProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
+import RefuelingForm from './components/RefuelingForm';
+import RefuelingHistory from './components/RefuelingHistory';
 
 import './App.css';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
+    <GlobalProvider>
     <div>
       <Header />
+      <div className='container'>
+        <RefuelingForm />
+        <RefuelingHistory />
+      </div>
     </div>
+    </GlobalProvider>
   );
 }
 
