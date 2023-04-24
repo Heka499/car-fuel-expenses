1afc1027d87b9f05f1d5b5e3405c396ddc24aaf0
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2023-04-24 16:10:58 +0300

    removed useless code

--- src/components/CarTotalCost.js
+++ src/components/CarTotalCost.js
@@ -5,50 +5,30 @@ import './TotalCost.css'
 const TotalCost = ({ car }) => {
   const { refuels } = useContext(GlobalContext);
 
-  const filteredRefuels = refuels.filter(refuel => refuel.car === car);
-
-  const liters = filteredRefuels.map(refuel => Number(refuel.liters));
-  const price = filteredRefuels.map(refuel => Number(refuel.price));
-  const distance = filteredRefuels.map(refuel => Number(refuel.distance));
-
-  const kWhs = refuels.map(refuels => Number(refuels.kWhs))
-  const priceElec = refuels.map(refuels => Number(refuels.priceElec))
-  const distanceElec = refuels.map(refuels => Number(refuels.distance))
-
-  const isElectric = refuels.map(refuels => (refuels.isElectric))
+  const liters = refuels.map(refuels => Number(refuels.liters))
+  const price = refuels.map(refuels => Number(refuels.price))
+  const distanceGas = refuels.map(refuels => Number(refuels.distance))
 
   const totalLiters = liters
     .filter(item => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
-
-  const totalPrice = price
+  
+    const totalPrice = price
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+    
+  const totalDistanceGas = distanceGas
     .filter(item => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
-  const totalDistance = distance
-    .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
+  const averageGasPrice = Number(totalPrice) / Number(totalLiters)
+
+  const averageExpensesGas = ((totalPrice / totalDistanceGas) * 100)
     .toFixed(2);
-
-  const totalkWhs = kWhs
-    .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-
-  const totalPriceElec = priceElec
-    .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-
-  const avgPriceElec = Number(totalPriceElec) / priceElec.length;
-  const totalPriceElecAll = Number(totalkWhs) * Number(avgPriceElec)
-
-  const averageExpenses = ((totalPrice / totalDistance) * 100)
-    .toFixed(2);
-
-  const averageConsumption = ((totalLiters / totalDistance) * 100)
+  const averageConsumptionGas = ((totalLiters / totalDistanceGas) * 100)
     .toFixed(2);
 
   return (
@@ -56,49 +36,30 @@ const TotalCost = ({ car }) => {
       <div className='total-cost-container'>
         <h4 className='total-cost-title'>{car}</h4>
           <div className='total-cost-info'>
-            {isElectric ? (
-              <>
-                <div className='total-cost-info-item'>
-                  <h4>Total Sum:</h4>
-                  <p>{totalPriceElecAll} €</p>
+                <div className="total-cost-info-item">
+                    <h4>Total Sum:</h4>
+                    <p>{totalPrice} €</p>
                 </div>
-                <div className='total-cost-info-item'>
-                  <h4>Total Charging:</h4>
-                  <p>{totalkWhs} kWh</p>
+                <div className="total-cost-info-item">
+                    <h4>Average Gas Price:</h4>
+                    <p>{averageGasPrice} €/Liter</p>
                 </div>
-              </>
-            ) : (
-              <>
-              <div className='total-cost-info-item'>
-                <h4>Total Sum:</h4>
-                <p>{totalPrice} €</p>
-              </div>
-              <div className='total-cost-info-item'>
-                <h4>Total Consumption:</h4>
-                <p>{totalLiters} Liters</p>
-              </div>
-              </>
-            )}
-            <div className='total-cost-info-item'>
-              <h4>Total Sum:</h4>
-              <p>{totalPrice} €</p>
-            </div>
-            <div className='total-cost-info-item'>
-              <h4>Total Consumption:</h4>
-              <p>{totalLiters} Liters</p>
-            </div>
-            <div className='total-cost-info-item'>
-              <h4>Total Distance</h4>
-              <p>{totalDistance} km</p>
-            </div>
-            <div className='total-cost-info-item'>
-              <h4>Average Expenses</h4>
-              <p>{averageExpenses} € per 100km</p>
-            </div>
-            <div className='total-cost-info-item'>
-              <h4>Average Consumption</h4>
-              <p>{averageConsumption} Liters per 100km</p>
-            </div>
+                <div className="total-cost-info-item">
+                    <h4>Total Consumption:</h4>
+                    <p>{totalLiters} Liters</p>
+                </div>
+                <div className="total-cost-info-item">
+                    <h4>Total Distance</h4>
+                    <p>{totalDistanceGas} km</p>
+                </div>
+                <div className="total-cost-info-item">
+                    <h4>Average Expenses</h4>
+                    <p>{averageExpensesGas} € per 100km</p>
+                </div>
+                <div className="total-cost-info-item">
+                    <h4>Average Consumption</h4>
+                    <p>{averageConsumptionGas} Liters per 100km</p>
+                </div>
           </div>
       </div>
     </>
